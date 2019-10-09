a3101078b3d2067e28aa888dfe54558607035d76
StellarDay <steve@spaceboat.space>
2019-10-08 22:25:05 -0500

    upgrade to https

--- js/justified-gallery.min.js
+++ js/justified-gallery.min.js
@@ -1,6 +1,6 @@
 /*!
  * Justified Gallery - v3.6.3
- * http://miromannino.github.io/Justified-Gallery/
+ * https://miromannino.github.io/Justified-Gallery/
  * Copyright (c) 2016 Miro Mannino
  * Licensed under the MIT license.
  */
